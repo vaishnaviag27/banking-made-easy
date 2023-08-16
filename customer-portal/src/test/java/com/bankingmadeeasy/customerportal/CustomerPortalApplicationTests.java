c92242c72f50cafcadef237901291e3aa185d254
Vaishnavi Agarwal <vaish.agarwal@gmail.com>
2023-08-16 22:18:58 +0530

    initialized project

---
+++ customer-portal/src/test/java/com/bankingmadeeasy/customerportal/CustomerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingmadeeasy.customerportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
